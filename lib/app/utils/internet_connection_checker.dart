95f0698d548dad005de630b4789d997f65d7bb61
MangeshKakade <mangeshkakade303@gmail.com>
2023-11-06 15:09:47 +0530

    Updated

--- lib/app/utils/internet_connection_checker.dart
+++ lib/app/utils/internet_connection_checker.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:connectivity/connectivity.dart';
 
-class InternetConnectionChecker extends StatelessWidget {
-  final Widget child; // Your regular content
-  final Widget noInternetWidget;
+import '../presentation/blocs/splash_bloc.dart';
+import '../presentation/pages/splash_screen.dart';
 
-  InternetConnectionChecker({required this.child, required this.noInternetWidget});
+class InternetConnectionChecker extends StatelessWidget {
+  final SplashBloc splashBloc;
+
+  InternetConnectionChecker({required this.splashBloc});
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<ConnectivityResult>(
       stream: Connectivity().onConnectivityChanged,
       builder: (context, snapshot) {
-        if (snapshot.data == ConnectivityResult.none) {
-          return noInternetWidget;
+        if (!snapshot.hasData || snapshot.data == ConnectivityResult.none) {
+          return Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.signal_wifi_off,
+                    size: 60,
+                    color: Colors.red,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "No Internet Connection",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                ],
+              ),
+            ),
+          );
         } else {
-          return child;
+          return SplashScreen(splashBloc: splashBloc);
         }
       },
     );
